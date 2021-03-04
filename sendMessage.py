e8196f96814021cf7668e14f9d1cac76395b72c9
Eric R <n644dc@gmail.com>
2021-03-04 04:41:26 +0000

    sending vehicle data now

--- sendMessage.py
+++ sendMessage.py
@@ -4,20 +4,32 @@ import os
 import time
 import json
 from Hologram.HologramCloud import HologramCloud
+from vehicleData import Vehicle
 
 
 erisCloudTestKey = "6cc9647209d1f184552da4ae39f12443410313d262676ac3428f68547792a16e"
 rainDropId = "rd0"
 
 hologram = HologramCloud(dict(), network='cellular')
-conn_result = hologram.network.connect()
+
+try:
+    conn_result = hologram.network.connect()
+except Exception as e:
+    pass
+
 
 if conn_result == False:
     print('Failed to connect to cell network')
 print("Connected to Cell Network")
 
-requestType = "REGISTER_NEW_DROP"
-requestBody = "New Rain Drop to Register"
+
+vehicle = Vehicle()
+vd = vehicle.getAllVehicleData()
+vehicleDataJson = json.dumps(vd)
+
+
+requestType = "VEHICLE_DATA"
+requestBody = vehicleDataJson
 payload = {"rainDropId": rainDropId, "requestType": requestType, "requestBody": requestBody}
 
 
@@ -25,7 +37,7 @@ print("sending Message...")
 message = json.dumps(payload)
 
 
-response_code = hologram.sendMessage(message, topics=["heartbeat", "rainDrop"])
+response_code = hologram.sendMessage(message, topics=["vehicleData", "rainDrop"])
 print(hologram.getResultString(response_code))
 print("Sent.")
 
---
+++ vehicleData.py
@@ -0,0 +1,38 @@
+from dronekit import connect
+
+
+
+class Vehicle:
+    lastDataSnapshot = None
+
+    def __init__(self):
+        pass
+    
+    def getAllVehicleData(self):
+        vehicle = connect('/dev/serial0', wait_ready=True, baud=921600)
+
+        self.lastDataSnapshot = [
+            {"px4_autopilot_version": str(vehicle.version)},
+            {"autopilot_ftp":         str(vehicle.capabilities.ftp)},
+            {"globalLoc":             str(vehicle.location.global_frame)},
+            {"globalLoc_relAlt":      str(vehicle.location.global_relative_frame)},
+            {"localLoc":              str(vehicle.location.local_frame)},
+            {"attitude":              str(vehicle.attitude)},
+            {"velocity":              str(vehicle.velocity)},
+            {"gps":                   str(vehicle.gps_0)},
+            {"groundspeed":           str(vehicle.groundspeed)},
+            {"airspeed":              str(vehicle.airspeed)},
+            {"gimbalStatus":          str(vehicle.gimbal)},
+            {"battery":               str(vehicle.battery)},
+            {"ekf_ok":                str(vehicle.ekf_ok)},
+            {"last_heartbeat":        str(vehicle.last_heartbeat)},
+            {"rangefinder":           str(vehicle.rangefinder)},
+            {"rangefinder_distance":  str(vehicle.rangefinder.distance)},
+            {"rangefinder_voltage":   str(vehicle.rangefinder.voltage)},
+            {"heading":               str(vehicle.heading)},
+            {"isArmable":             str(vehicle.is_armable)},
+            {"systemStatus":          str(vehicle.system_status.state)},
+            {"mode":                  str(vehicle.mode.name)},
+            {"armed":                 str(vehicle.armed)}
+        ]
+        return self.lastDataSnapshot
